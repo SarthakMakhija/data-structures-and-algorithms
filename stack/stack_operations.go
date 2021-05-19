5a188391e79c530794c78b41b1a4f234966484d1
sarthak.makhija <sarthak.makhija@gmail.com>
2021-05-19 14:00:13 +0530

    Sarthak | Adds postfix evaluator

--- stack/stack_operations.go
+++ stack/stack_operations.go
@@ -1,34 +1,41 @@
 package stack
 
+import (
+	"errors"
+	"strconv"
+)
+
 func MatchParentheses(expression string) bool {
 	if len(expression) == 0 {
 		return false
 	}
 
-	var stack_push func(rune)
-	var stack_pop func()
+	var stack_push func(string)
+	var stack_pop func() string
 
 	var stack_top = 0
-	var stack []rune
+	var stack []string
 
-	stack_push = func(element rune) {
+	stack_push = func(element string) {
 		stack = append(stack, element)
 		stack_top = stack_top + 1
 	}
 
-	stack_pop = func() {
+	stack_pop = func() string {
 		stack_top = stack_top - 1
 		if stack_top < 0 {
-			return
+			return ""
 		}
+		top := stack[stack_top]
 		stack = stack[0:stack_top]
+		return top
 	}
 
 	for index := 0; index < len(expression); index++ {
-		char := rune(expression[index])
-		if char == '(' {
-			stack_push(char)
-		} else if char == ')' {
+		element := string(expression[index])
+		if element == "(" {
+			stack_push(element)
+		} else if element == ")" {
 			stack_pop()
 		}
 	}
@@ -38,4 +45,56 @@ func MatchParentheses(expression string) bool {
 	} else {
 		return false
 	}
+}
+
+func PostFixEvaluate(expression string) (int, error) {
+	if len(expression) == 0 {
+		return 0, nil
+	}
+
+	var stack_push func(int)
+	var stack_pop func() int
+
+	var stack_top = 0
+	var stack []int
+
+	stack_push = func(element int) {
+		stack = append(stack, element)
+		stack_top = stack_top + 1
+	}
+
+	stack_pop = func() int {
+		stack_top = stack_top - 1
+		if stack_top < 0 {
+			return -1
+		}
+		top := stack[stack_top]
+		stack = stack[0:stack_top]
+		return top
+	}
+
+	for index := 0; index < len(expression); index++ {
+		element := string(expression[index])
+		if v, err := strconv.Atoi(element); err == nil {
+			stack_push(v)
+		}
+		if element == "+" || element == "*" || element == "-" || element == "/" {
+			operand1 := stack_pop()
+			operand2 := stack_pop()
+			if operand1 == -1 || operand2 == -1 {
+				return -1, errors.New("incorrect postfix expressions")
+			}
+
+			if element == "+" {
+				stack_push(operand1 + operand2)
+			} else if element == "*" {
+				stack_push(operand1 * operand2)
+			} else if element == "-" {
+				stack_push(operand2 - operand1)
+			} else if element == "/" {
+				stack_push(operand2 / operand1)
+			}
+		}
+	}
+	return stack[0], nil
 }
--- stack/stack_operations_test.go
+++ stack/stack_operations_test.go
@@ -53,3 +53,51 @@ func TestParenthesesNotMatching_4(t *testing.T) {
 		t.Fatalf(`Expected false but found %v`, does_parentheses_match)
 	}
 }
+
+func TestEvaluatePostfixExpression_1(t *testing.T) {
+
+	result, _ := stack.PostFixEvaluate("12+")
+	if result != 3 {
+		t.Fatalf(`Expected 3 but found %v`, result)
+	}
+}
+
+func TestEvaluatePostfixExpression_2(t *testing.T) {
+
+	result, _ := stack.PostFixEvaluate("12+3*")
+	if result != 9 {
+		t.Fatalf(`Expected 9 but found %v`, result)
+	}
+}
+
+func TestEvaluatePostfixExpression_3(t *testing.T) {
+
+	result, _ := stack.PostFixEvaluate("53+82-*")
+	if result != 48 {
+		t.Fatalf(`Expected 48 but found %v`, result)
+	}
+}
+
+func TestEvaluatePostfixExpression_4(t *testing.T) {
+
+	result, _ := stack.PostFixEvaluate("53+82-*6/")
+	if result != 8 {
+		t.Fatalf(`Expected 8 but found %v`, result)
+	}
+}
+
+func TestEvaluatePostfixExpression_5(t *testing.T) {
+
+	result, _ := stack.PostFixEvaluate("234*+5+")
+	if result != 19 {
+		t.Fatalf(`Expected 19 but found %v`, result)
+	}
+}
+
+func TestEvaluateInvalidPostfixExpression(t *testing.T) {
+
+	_, err := stack.PostFixEvaluate("+12")
+	if err == nil {
+		t.Fatal("Expected error but did not have any error")
+	}
+}
