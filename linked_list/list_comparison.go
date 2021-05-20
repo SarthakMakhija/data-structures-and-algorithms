62ec22ee2c9089dc2d2e9fb9d68acc8cd80e91ef
sarthak.makhija <sarthak.makhija@gmail.com>
2021-05-20 11:58:25 +0530

    Sarthak | Adds linkedlist comparison, does a comparison by taking 4 elements at a time. Implementation uses 4 variables, can be converted to an array

---
+++ linked_list/list_comparison.go
@@ -0,0 +1,83 @@
+package linkedlist
+
+import (
+	"errors"
+)
+
+//Assume lists are of the same size
+func Compare(list1 *LinkedList, list2 *LinkedList) bool {
+
+	if list1.first == nil || list2.first == nil {
+		return false
+	}
+
+	matched := true
+
+	for list1_current := list1.first; list1_current != nil; {
+		v1 := list1_current.value
+		v1_matched := false
+
+		var v2_matched, v3_matched, v4_matched bool
+
+		v2, err := value_of(list1_current, 1)
+		if err == nil {
+			v2_matched = false
+		} else {
+			v2_matched = true
+		}
+
+		v3, err := value_of(list1_current, 2)
+		if err == nil {
+			v3_matched = false
+		} else {
+			v3_matched = true
+		}
+
+		v4, err := value_of(list1_current, 3)
+		if err == nil {
+			v4_matched = false
+		} else {
+			v4_matched = true
+		}
+
+		for list2_current := list2.first; list2_current != nil; list2_current = list2_current.next {
+
+			if list2_current.value == v1 {
+				v1_matched = true
+			} else if list2_current.value == v2 {
+				v2_matched = true
+			} else if list2_current.value == v3 {
+				v3_matched = true
+			} else if list2_current.value == v4 {
+				v4_matched = true
+			}
+		}
+
+		if v1_matched && v2_matched && v3_matched && v4_matched {
+			list1_current = advance(list1_current, 4)
+		} else {
+			matched = false
+			break
+		}
+	}
+
+	return matched
+}
+
+func value_of(head *Node, by int) (int, error) {
+	node := advance(head, by)
+	if node == nil {
+		return -1, errors.New("NO VALUE POSSIBLE")
+	}
+	return node.value, nil
+}
+
+func advance(head *Node, by int) *Node {
+	for count := 1; count <= by; count++ {
+		head = head.next
+		if head == nil {
+			break
+		}
+	}
+	return head
+}
---
+++ linked_list/list_comparison_test.go
@@ -0,0 +1,118 @@
+package linkedlist_test
+
+import (
+	"testing"
+
+	linkedlist "example.com/data-structures-and-algorithms/linked_list"
+)
+
+func TestCompareListsWithSameElementsInDifferentOrder_1(t *testing.T) {
+
+	list1 := linkedlist.LinkedList{}
+	list2 := linkedlist.LinkedList{}
+
+	list1.Add(1)
+	list1.Add(2)
+	list1.Add(3)
+
+	list2.Add(3)
+	list2.Add(1)
+	list2.Add(2)
+
+	matched := linkedlist.Compare(&list1, &list2)
+	if matched != true {
+		t.Fatalf("Expected true but found %v", matched)
+	}
+}
+
+func TestCompareListsWithSameElementsInDifferentOrder_2(t *testing.T) {
+
+	list1 := linkedlist.LinkedList{}
+	list2 := linkedlist.LinkedList{}
+
+	list1.Add(1)
+	list1.Add(2)
+	list1.Add(10)
+	list1.Add(9)
+	list1.Add(8)
+
+	list2.Add(10)
+	list2.Add(9)
+	list2.Add(8)
+	list2.Add(1)
+	list2.Add(2)
+
+	matched := linkedlist.Compare(&list1, &list2)
+	if matched != true {
+		t.Fatalf("Expected true but found %v", matched)
+	}
+}
+
+func TestCompareListsWithDifferentElements_1(t *testing.T) {
+
+	list1 := linkedlist.LinkedList{}
+	list2 := linkedlist.LinkedList{}
+
+	list1.Add(1)
+	list1.Add(2)
+	list1.Add(10)
+	list1.Add(9)
+	list1.Add(8)
+
+	list2.Add(10)
+	list2.Add(90)
+	list2.Add(8)
+	list2.Add(1)
+	list2.Add(2)
+
+	matched := linkedlist.Compare(&list1, &list2)
+	if matched != false {
+		t.Fatalf("Expected false but found %v", matched)
+	}
+}
+
+func TestCompareListsWithDifferentElements_2(t *testing.T) {
+
+	list1 := linkedlist.LinkedList{}
+	list2 := linkedlist.LinkedList{}
+
+	list1.Add(1)
+	list1.Add(2)
+	list1.Add(2)
+
+	list2.Add(1)
+	list2.Add(2)
+	list2.Add(3)
+
+	matched := linkedlist.Compare(&list1, &list2)
+	if matched != false {
+		t.Fatalf("Expected false but found %v", matched)
+	}
+}
+
+func TestCompareListsWithSameElementsInDifferentOrder_3(t *testing.T) {
+
+	list1 := linkedlist.LinkedList{}
+	list2 := linkedlist.LinkedList{}
+
+	list1.Add(1)
+	list1.Add(2)
+	list1.Add(10)
+	list1.Add(9)
+	list1.Add(8)
+	list1.Add(7)
+	list1.Add(6)
+
+	list2.Add(10)
+	list2.Add(9)
+	list2.Add(8)
+	list2.Add(6)
+	list2.Add(7)
+	list2.Add(1)
+	list2.Add(2)
+
+	matched := linkedlist.Compare(&list1, &list2)
+	if matched != true {
+		t.Fatalf("Expected true but found %v", matched)
+	}
+}
