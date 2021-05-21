c11fa8dbf0510f8bddbb0be02bf66736890b70bf
sarthak.makhija <sarthak.makhija@gmail.com>
2021-05-21 11:37:02 +0530

    Sarthak | Refactors string operations

--- strings_ops/string_operations.go
+++ strings_ops/string_operations.go
@@ -1,7 +1,6 @@
 package stringsops
 
 import (
-	"fmt"
 	"math"
 	"strings"
 )
@@ -9,51 +8,58 @@ import (
 func Duplicates(str string) []string {
 
 	length := len(str)
-	lowercase := strings.ToLower(str)
-
 	if length == 0 {
 		return nil
 	}
+	lowercase := strings.ToLower(str)
 
 	type CharacterOccurrence struct {
-		ch         string
-		occurrence int
+		ch    string
+		count int
 	}
 	var occurrences []CharacterOccurrence = make([]CharacterOccurrence, length)
 	var duplicates []string
+
+	is_occurrence_slot_available := func(index int) bool {
+		return len(occurrences[index].ch) == 0
+	}
+	add_occurrence := func(ch byte, index int) {
+		occurrences[index] = CharacterOccurrence{
+			ch:    string(ch),
+			count: 1,
+		}
+	}
+	slot_after := func(index int, ch byte) int {
+		existing := occurrences[index]
+		if existing.ch == string(ch) {
+			return index
+		}
+		for count := 0; count < length; count++ {
+			if is_occurrence_slot_available(count) || occurrences[count].ch == string(ch) {
+				return count
+			}
+		}
+		return -1
+	}
 
 	for index := 0; index < length; index++ {
 		ch := lowercase[index]
 		occurrence_index := int(math.Mod(float64(ch), float64(length)))
 
-		if len(occurrences[occurrence_index].ch) == 0 {
-			occurrences[occurrence_index] = CharacterOccurrence{
-				ch:         string(ch),
-				occurrence: 1,
-			}
+		if is_occurrence_slot_available(occurrence_index) {
+			add_occurrence(ch, occurrence_index)
 		} else {
-			existing := occurrences[occurrence_index]
-
-			if existing.ch == string(ch) {
-				occurrences[occurrence_index].occurrence = occurrences[occurrence_index].occurrence + 1
-			} else {
-				for index := 0; index < length; index++ {
-					if len(occurrences[index].ch) == 0 || occurrences[index].ch == string(ch) {
-						occurrences[index] = CharacterOccurrence{
-							ch:         string(ch),
-							occurrence: occurrences[index].occurrence + 1,
-						}
-						break
-					}
-				}
+			next_slot := slot_after(occurrence_index, ch)
+			occurrences[next_slot] = CharacterOccurrence{
+				ch:    string(ch),
+				count: occurrences[next_slot].count + 1,
 			}
 		}
 	}
 
-	fmt.Println(occurrences)
-	for index := 0; index < length; index++ {
-		if occurrences[index].occurrence > 1 {
-			duplicates = append(duplicates, occurrences[index].ch)
+	for _, occurrence := range occurrences {
+		if occurrence.count > 1 {
+			duplicates = append(duplicates, occurrence.ch)
 		}
 	}
 	return duplicates
