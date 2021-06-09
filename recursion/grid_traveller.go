011d82e0c36210a4ec08bf363f8b5d30621f1da4
sarthak.makhija <sarthak.makhija@gmail.com>
2021-06-10 00:54:43 +0530

    Sarthak | Implements first cut of memoization in grid traveller

--- recursion/grid_traveller.go
+++ recursion/grid_traveller.go
@@ -2,7 +2,12 @@ package recursion
 
 func PossibleWaysToTravelEndOfGridSized(rows int, columns int) int {
 
+	type Position struct {
+		row int
+		col int
+	}
 	endRowPosition, endColumnPosition := rows-1, columns-1
+	waysToTravelByPosition := make(map[Position]int)
 
 	isRightMoveAllowed := func(columnPosition int) bool {
 		return columnPosition < columns-1
@@ -13,17 +18,24 @@ func PossibleWaysToTravelEndOfGridSized(rows int, columns int) int {
 
 	var waysPossibleToTravelGridSizedInner func(int, int) int
 	waysPossibleToTravelGridSizedInner = func(rowPosition int, columnPosition int) int {
+		waysPossible, isPresent := waysToTravelByPosition[Position{row: rowPosition, col: columnPosition}]
+		if isPresent {
+			return waysPossible
+		}
 		if rowPosition == endRowPosition && columnPosition == endColumnPosition {
 			return 1
 		}
 		if isRightMoveAllowed(columnPosition) && isBottomMoveAllowed(rowPosition) {
-			return waysPossibleToTravelGridSizedInner(rowPosition, columnPosition+1) +
+			totalWays := waysPossibleToTravelGridSizedInner(rowPosition, columnPosition+1) +
 				waysPossibleToTravelGridSizedInner(rowPosition+1, columnPosition)
+			waysToTravelByPosition[Position{row: rowPosition, col: columnPosition}] = totalWays
+			return totalWays
 		} else if isRightMoveAllowed(columnPosition) {
 			return waysPossibleToTravelGridSizedInner(rowPosition, columnPosition+1)
 		} else {
 			return waysPossibleToTravelGridSizedInner(rowPosition+1, columnPosition)
 		}
 	}
-	return waysPossibleToTravelGridSizedInner(0, 0)
+	totalWays := waysPossibleToTravelGridSizedInner(0, 0)
+	return totalWays
 }
