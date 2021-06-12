275bcfe6e8bdc10474f723b58686a7ea65407f14
sarthak.makhija <sarthak.makhija@gmail.com>
2021-06-12 20:06:54 +0530

    Sarthak | Implements sorted stack using an additional stack

--- stack/green_book/operations.go
+++ stack/green_book/operations.go
@@ -1,9 +1,18 @@
 package green_book
 
+import (
+	"github.com/SarthakMakhija/data-structures-and-algorithms/stack"
+)
+
 type Stack struct {
 	elements [10]int
 	top      int
 	minIndex int
+}
+
+type SortedStack struct {
+	elements [10]int
+	top      int
 }
 
 //Push
@@ -29,4 +38,37 @@ func (s *Stack) Pop() int {
 
 func (s *Stack) Min() int {
 	return s.elements[s.minIndex]
+}
+
+//Push
+//Assume no overflow
+func (s *SortedStack) Push(element int) {
+	add := func(e int) {
+		s.elements[s.top] = e
+		s.top = s.top + 1
+	}
+	if s.top == 0 || s.elements[s.top-1] > element {
+		add(element)
+	} else {
+		backUpStack := stack.IntStack{}
+		for s.top > 0 && s.elements[s.top-1] < element {
+			e := s.Pop()
+			backUpStack.Push(e)
+		}
+		add(element)
+		e := backUpStack.Pop()
+		for e != -1 {
+			add(e)
+			e = backUpStack.Pop()
+		}
+	}
+}
+
+func (s *SortedStack) Pop() int {
+	if s.top == 0 {
+		return -1
+	}
+	element := s.elements[s.top-1]
+	s.top = s.top - 1
+	return element
 }
--- stack/green_book/operations_test.go
+++ stack/green_book/operations_test.go
@@ -2,6 +2,7 @@ package green_book_test
 
 import (
 	"github.com/SarthakMakhija/data-structures-and-algorithms/stack/green_book"
+	"reflect"
 	"testing"
 )
 
@@ -38,5 +39,52 @@ func TestStackPop_1(t *testing.T) {
 	pop := stack.Pop()
 	if pop != 20 {
 		t.Fatalf("Expected top to be 20 but found %v", pop)
+	}
+}
+
+func TestSortedStackPush_1(t *testing.T) {
+	stack := green_book.SortedStack{}
+	stack.Push(1)
+	stack.Push(2)
+	stack.Push(3)
+	stack.Push(4)
+
+	elements := []int{stack.Pop(), stack.Pop(), stack.Pop(), stack.Pop()}
+	expected := []int{1, 2, 3, 4}
+
+	if !reflect.DeepEqual(elements, expected) {
+		t.Fatalf("Expected %v received %v", expected, elements)
+	}
+}
+
+func TestSortedStackPush_2(t *testing.T) {
+	stack := green_book.SortedStack{}
+	stack.Push(40)
+	stack.Push(5)
+	stack.Push(1)
+	stack.Push(50)
+	stack.Push(7)
+	stack.Push(6)
+
+	elements := []int{stack.Pop(), stack.Pop(), stack.Pop(), stack.Pop(), stack.Pop(), stack.Pop()}
+	expected := []int{1, 5, 6, 7, 40, 50}
+
+	if !reflect.DeepEqual(elements, expected) {
+		t.Fatalf("Expected %v received %v", expected, elements)
+	}
+}
+
+func TestSortedStackPush_3(t *testing.T) {
+	stack := green_book.SortedStack{}
+	stack.Push(4)
+	stack.Push(3)
+	stack.Push(2)
+	stack.Push(1)
+
+	elements := []int{stack.Pop(), stack.Pop(), stack.Pop(), stack.Pop()}
+	expected := []int{1, 2, 3, 4}
+
+	if !reflect.DeepEqual(elements, expected) {
+		t.Fatalf("Expected %v received %v", expected, elements)
 	}
 }
