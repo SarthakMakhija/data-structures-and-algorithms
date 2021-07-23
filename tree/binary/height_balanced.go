b725fdd5b6b1861336439cbc35c4c5c699727d7b
sarthak.makhija <sarthak.makhija@gmail.com>
2021-07-23 20:43:29 +0530

    Sarthak | Implements if a tree is height unbalanced by K

--- tree/binary/height_balanced.go
+++ tree/binary/height_balanced.go
@@ -1,5 +1,9 @@
 package binarytree
 
+import (
+	"math"
+)
+
 func (t StringBinaryTree) IsHeightBalanced() bool {
 
 	var isHeightBalancedInner func(node *StringNode) (bool, int)
@@ -22,4 +26,32 @@ func (t StringBinaryTree) IsHeightBalanced() bool {
 	}
 	isHeightBalanced, _ := isHeightBalancedInner(t.Root)
 	return isHeightBalanced
+}
+
+func (t StringBinaryTree) NodeWithHeightUnbalancedByK(k int) *StringNode {
+
+	var unbalancedNode *StringNode = nil
+	var isHeightBalancedInner func(node *StringNode) (bool, int)
+	isHeightBalancedInner = func(node *StringNode) (bool, int) {
+		if node == nil {
+			return true, 0
+		}
+		isHeightBalancedLeft, depthLeft := isHeightBalancedInner(node.Left)
+		isHeightBalancedRight, depthRight := isHeightBalancedInner(node.Right)
+
+		if isHeightBalancedLeft && isHeightBalancedRight && math.Abs(float64(depthLeft)-float64(depthRight)) < float64(k) {
+			if depthLeft >= depthRight {
+				return true, depthLeft + 1
+			} else {
+				return true, depthRight + 1
+			}
+		} else {
+			if unbalancedNode == nil {
+				unbalancedNode = node
+			}
+			return false, -1
+		}
+	}
+	isHeightBalancedInner(t.Root)
+	return unbalancedNode
 }
--- tree/binary/height_balanced_test.go
+++ tree/binary/height_balanced_test.go
@@ -107,3 +107,62 @@ func TestIsHeightBalanced2(t *testing.T) {
 		t.Fatalf("Expected false, recevied %v", output)
 	}
 }
+
+func TestIsHeightBalancedByK1(t *testing.T) {
+	root := binarytree.StringNode{
+		Value: "A",
+		Left: &binarytree.StringNode{
+			Value: "B",
+			Left: &binarytree.StringNode{
+				Value: "C",
+				Left: &binarytree.StringNode{
+					Value: "D",
+				},
+				Right: &binarytree.StringNode{
+					Value: "G",
+				},
+			},
+			Right: &binarytree.StringNode{
+				Value: "F",
+				Right: &binarytree.StringNode{
+					Value: "G",
+					Left: &binarytree.StringNode{
+						Value: "H",
+					},
+				},
+			},
+		},
+		Right: &binarytree.StringNode{
+			Value: "I",
+			Left: &binarytree.StringNode{
+				Value: "J",
+				Right: &binarytree.StringNode{
+					Value: "K",
+					Left: &binarytree.StringNode{
+						Value: "L",
+						Right: &binarytree.StringNode{
+							Value: "M",
+						},
+					},
+					Right: &binarytree.StringNode{
+						Value: "N",
+					},
+				},
+			},
+			Right: &binarytree.StringNode{
+				Value: "O",
+				Right: &binarytree.StringNode{
+					Value: "P",
+				},
+			},
+		},
+	}
+	tree := binarytree.StringBinaryTree{
+		Root: &root,
+	}
+
+	node := tree.NodeWithHeightUnbalancedByK(3)
+	if node.Value != "J" {
+		t.Fatalf("Expected J, recevied %v", node.Value)
+	}
+}
