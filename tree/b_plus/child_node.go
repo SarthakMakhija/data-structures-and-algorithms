b6729103f4980f8efb209f4eacc85b6c517ac2f6
sarthak.makhija <sarthak.makhija@gmail.com>
2021-10-20 12:29:58 +0530

    Sarthak | Removes B+Tree

--- tree/b_plus/child_node.go
+++
@@ -1,9 +0,0 @@
-package b_plus
-
-func (childNodeReferences *ChildNodeReferences) InsertAt(index int, childNodeReference *StringBPlusTreeNode) {
-	*childNodeReferences = append(*childNodeReferences, childNodeReference)
-	if index < len(*childNodeReferences) {
-		copy((*childNodeReferences)[index+1:], (*childNodeReferences)[index:])
-	}
-	(*childNodeReferences)[index] = childNodeReference
-}
